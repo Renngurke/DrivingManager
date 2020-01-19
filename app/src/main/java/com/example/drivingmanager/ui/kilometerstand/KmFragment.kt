576f19b00486c6f80ee13d1457f222e5b1dabb6f
dillesc <51393867+dillesc@users.noreply.github.com>
2020-01-19 23:18:58 +0100

    Kilometerstand hinzufuegen

--- app/src/main/java/com/example/drivingmanager/ui/kilometerstand/KmFragment.kt
+++ app/src/main/java/com/example/drivingmanager/ui/kilometerstand/KmFragment.kt
@@ -1,23 +1,93 @@
 package com.example.drivingmanager.ui.kilometerstand
 
 import android.os.Bundle
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
+import android.text.Editable
+import android.text.TextWatcher
+import android.view.*
 import android.widget.Toast
 import androidx.fragment.app.Fragment
-import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProviders
 import com.example.drivingmanager.MainActivity
 import com.example.drivingmanager.R
 import kotlinx.android.synthetic.main.fragment_kmstand.*
 import kotlinx.android.synthetic.main.fragment_kmstand.view.*
-import kotlinx.android.synthetic.main.fragment_reisekosten.view.*
 
 class KmFragment : Fragment() {
 
     private lateinit var kmFragmentViewModel: KmViewModel
     private lateinit var root: View
+
+    //enable options menu in this fragment
+    override fun onCreate(savedInstanceState: Bundle?) {
+        setHasOptionsMenu(true)
+        super.onCreate(savedInstanceState)
+    }
+
+    //inflate the menu
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        inflater.inflate(R.menu.add_kmstand_menu, menu)
+        super.onCreateOptionsMenu(menu, inflater)
+    }
+
+    //handle item clicks of menu
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        if (item.itemId == R.id.savekm) {
+            var error = false
+
+            if (add_kmstand.text.toString().isNullOrEmpty()) {
+                add_kmstand.setError("Sie haben keine Kilometeranzahl eingegeben")
+                error = true
+            }
+            if (add_kmstadt.text.toString().isNullOrEmpty()) {
+                add_kmstadt.setError("Sie haben keine Kilometeranzahl eingegeben")
+                error = true
+            }
+            if (add_kmautobahn.text.toString().isNullOrEmpty()) {
+                add_kmautobahn.setError("Sie haben keine Kilometeranzahl eingegeben")
+                error = true
+            }
+            if (add_kmhybrid.text.toString().isNullOrEmpty()) {
+                add_kmhybrid.setError("Sie haben keine Kilometeranzahl eingegeben")
+                error = true
+            }
+
+            if (!error) {
+
+                var kmStadt = add_kmstadt.text.toString().toInt()
+                var kmAutobahn = add_kmautobahn.text.toString().toInt()
+                var kmHybrid = add_kmhybrid.text.toString().toInt()
+                var kmStand = add_kmstand.text.toString().toInt()
+
+                var car = MainActivity.cl.cars[MainActivity.cl.index]
+
+                if ((kmStadt + kmAutobahn + kmHybrid) < (kmStand - car.gesKM)) {
+                    val err = "Kilometeranzahl zu niedrig"
+                    add_kmstadt.setError(err)
+                } else {
+
+                    car.gesKM = kmStand
+                    car.kmStand_angeben(kmStadt, kmAutobahn, kmHybrid)
+                    MainActivity.cl.save(this.context!!)
+
+                    add_kmstadt.text = null
+                    add_kmautobahn.text = null
+                    add_kmhybrid.text = null
+                    add_kmstand.text = null
+
+                    kmstand_vorher.text = String.format(
+                        "Momentaner Kilometerstand: %d",
+                        MainActivity.cl.cars[MainActivity.cl.index].gesKM
+                    )
+                    Toast.makeText(
+                        this.context,
+                        "Der Kilometerstand wurde erfolgreich angegeben",
+                        Toast.LENGTH_LONG
+                    ).show()
+                }
+            }
+        }
+        return super.onOptionsItemSelected(item)
+    }
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -28,36 +98,36 @@ class KmFragment : Fragment() {
             ViewModelProviders.of(this).get(KmViewModel::class.java)
         root = inflater.inflate(R.layout.fragment_kmstand, container, false)
 
-        /*
-        if(root.add_kmstand.text.toString().isNotEmpty()) {
-            var index: Int = MainActivity.cl.index
-            var kmdifferenz = add_kmstand.text.toString().toInt() - MainActivity.cl.cars[index].gesKM
-            root.km_diff.text = kmdifferenz.toString()
-        }
-         */ //Dynamisch die Kilometerdifferenzt anzeigen lassen, crasht die App momentan
+        val index = MainActivity.cl.index
+        root.kmstand_vorher.text =
+            String.format("Momentaner Kilometerstand: %d", MainActivity.cl.cars[index].gesKM)
 
-        root.buttonKms.setOnClickListener {
-            if (add_kmstadt.text.toString().isNotEmpty()
-                && add_kmstadt.text.toString().isNotEmpty()
-                && add_kmhybrid.text.toString().isNotEmpty() ) {
-
-                var kmStadt = add_kmstadt.text.toString().toInt()
-                var kmAutobahn = add_kmautobahn.text.toString().toInt()
-                var kmHybrid = add_kmhybrid.text.toString().toInt()
-
-                var car = MainActivity.cl.cars[MainActivity.cl.index]
-
-                car.kmStand_angeben(kmStadt, kmAutobahn, kmHybrid)
-
-                add_kmstadt.text = null
-                add_kmautobahn.text = null
-                add_kmhybrid.text = null
-
-                /*
-                Toast.makeText(this, "Der Kilometerstand wurde erfolgreich angegeben", Toast.LENGTH_LONG).show()
-                 */ //Toast Nachricht funktioniert irgendwie nicht
+        root.add_kmstand.addTextChangedListener(object : TextWatcher {
+            override fun afterTextChanged(s: Editable?) {
             }
-        }
+
+            override fun beforeTextChanged(
+                s: CharSequence, start: Int,
+                count: Int, after: Int
+            ) {
+            }
+
+            override fun onTextChanged(
+                s: CharSequence, start: Int,
+                before: Int, count: Int
+            ) {
+                if (add_kmstand.text.toString().isNullOrEmpty()) {
+                    km_diff.text = null
+                } else if (add_kmstand.text.toString().toInt() <= MainActivity.cl.cars[index].gesKM) {
+                    add_kmstand.setError("Neuer Kilometerstand ist kleiner als der bisherige")
+                } else {
+                    km_diff.text = String.format(
+                        "%d",
+                        (add_kmstand.text.toString().toInt() - MainActivity.cl.cars[index].gesKM)
+                    )
+                }
+            }
+        })
 
         return root
     }
